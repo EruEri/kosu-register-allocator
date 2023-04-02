0a0c4b3c6c2a8d53b67f4f716ae6dafac8e08175
EruEri <ndiayeyves@gmail.com>
2023-04-02 06:07:20 +0200

    [Graph]: new implemenation of colored base

--- lib/graph.ml
+++ lib/graph.ml
@@ -81,125 +81,94 @@ end
 
 module ColoredMake(S: OrderedType) (Color: ColoredType) = struct
 
-  module SColored = struct
-    type colored_t = {
-      node: S.t;
-      color: Color.color option
-    }
+  type colored_node = {
+    node: S.t;
+    color: Color.color option
+  }
 
-    type t = colored_t
+  type egde = {
+    root: S.t;
+    along: S.t
+  }
 
-    let compare_opt lhs rhs = 
-      match lhs, rhs with
-      | Some lcolor, Some rcolor -> Color.compare lcolor rcolor
-      | _ -> compare lhs rhs
+  let compare_opt lhs rhs = 
+    match lhs, rhs with
+    | Some lcolor, Some rcolor -> Color.compare lcolor rcolor
+    | _ -> compare lhs rhs
+
+
+  exception Unexisting_node of S.t
+  exception Link_same_color of colored_node * colored_node
+
+  module Nodes = Set.Make(struct
+    type t = colored_node
+
+    let compare lhs rhs = S.compare lhs.node rhs.node
+  end)
+
+  module Edges = Set.Make(struct
+    type t = egde
 
     let compare lhs rhs = 
-      let s_compare = S.compare lhs.node rhs.node in
-      if s_compare <> 0 then  s_compare
-      else compare_opt lhs.color rhs.color
-  end
+      let f_comp = S.compare lhs.root rhs.root in
+      if f_comp <> 0 then f_comp
+      else
+        S.compare lhs.along rhs.along
+  end)
 
-  module ColoredGraph = Make(SColored)
-  module NoColoredGraph = Make(S)
-
-  exception Link_same_color
-
-  type graph = NoColoredGraph.graph
-  type colored_graph = ColoredGraph.graph
-
-  let make_node color elt = 
-    let open SColored in
-  {
-    color = Some color;
-    node = elt
+  type colored_graph = {
+    nodes: Nodes.t;
+    egdes: Edges.t
   }
 
-  let make_node_opt color elt = 
-    let open SColored in
-  {
-    color = color;
-    node = elt
+  let empty = {
+    nodes = Nodes.empty;
+    egdes = Edges.empty;
   }
 
-  let compare_cnode_as_node lhs rhs = 
-    let open SColored in
-    S.compare lhs.node rhs.node
+  let create_colored color node = 
+    {
+      node;
+      color
+    }
 
+  let create_edge root along = 
+    {
+      root = root.node;
+      along = along.node
+    }
 
-  let add_colored_node cnode cgraph = 
-    let new_graph =  ColoredGraph.add_node cnode cgraph in
-    if new_graph = cgraph then new_graph
-    else
-      cgraph |> ColoredGraph.map (fun set ->
-        set |> ColoredGraph.NodeSet.map (fun colored_node ->
-          if compare_cnode_as_node colored_node cnode = 0 then cnode else colored_node
-      )
-    )
+  let add_node node graph = 
+    {
+      graph with nodes = Nodes.add node graph.nodes
+    }
 
-  let add_node ?(color = None) elt cgraph =
-    let open SColored in 
-    let cnode = { node = elt; color } in
-    add_colored_node cnode cgraph
+  let add_edge egde graph = 
+    {
+      graph with egdes = Edges.add egde graph.egdes
+    }
 
+    let add_uncolored_node ?(color) node graph = 
+      add_node (create_colored color node) graph
 
+    let edges node cgraph = 
+      cgraph.egdes |> Edges.filter (fun edge ->
+        S.compare edge.root node = 0  
+      ) 
 
-  let egde_to = 
-    ColoredGraph.egde_to
+    let check_contains node graph = 
+      if not @@ Nodes.mem (create_colored None node) @@ graph.nodes then 
+        raise @@ Unexisting_node node
 
-    
-
-  let contains node = let open SColored in ColoredGraph.exist_node (fun colored_node -> 
-    S.compare colored_node.node node.node = 0
-  )
-
-  (**
-    @raise Not_found if [node] is not in [graph]    
-  *)
-  let check_contains node graph = 
-    if not @@ contains node graph then raise Not_found
-
-  let check_not_same_color lhs rhs = 
-    let open SColored in
-    let () = if SColored.compare_opt lhs.color rhs.color = 0 && Option.is_some lhs.color then raise Link_same_color
-    in
-    ()
-
-
-  (** 
-    Add an egde beetween [node] and [along] in [graph]. 
-    @raise Not_found if [node] or [along] aren't in the graph
-    @raise Link_same_color if [node] has the same color than [along] 
-  *)
-  let link (node: SColored.t) ~(along : SColored.t) (graph: colored_graph): colored_graph = 
-    let () = check_contains node graph in
-    let () = check_contains along graph in
-    let () = check_not_same_color node along in
-    let linked_to = egde_to node graph in
-    let extended_node = ColoredGraph.NodeSet.add along linked_to in
-    ColoredGraph.NodeMap.add node extended_node graph
-    let color_links ~precolored linked = 
-      linked |> List.map (fun node ->
-        let color = precolored |> List.find_map (fun (elt, color) -> if S.compare node elt = 0 then Some color else None ) in
-        make_node_opt color node 
-      )
-    let of_graph ?(precolored = ( []: (S.t * Color.color) list ) ) graph = 
-      let open SColored in
-       graph |> NoColoredGraph.bindings |> List.fold_left (fun graph_acc (node, linked) -> 
-        let colored_opt = precolored |> List.find_map (fun (elt, color) -> if S.compare node elt = 0 then Some color else None ) in
-        let cnode = make_node_opt colored_opt node in
-        let clinked = color_links ~precolored linked in
-        let graph_acc = add_colored_node cnode graph_acc in
-        let graph_acc = clinked |> List.fold_left (fun inner_graph cl -> 
-          let new_graph = inner_graph |> add_colored_node cl |> link cnode ~along:cl in
-          new_graph
-        ) graph_acc in
-        graph_acc
-      ) ColoredGraph.empty
-
-    let color_node color cnode cgraph = failwith ""
+    let check_color root along = 
+      if compare_opt root.color along.color = 0 && Option.is_some root.color then 
+        raise @@ Link_same_color (root, along)
       
-      (* graph |>  NoColoredGraph.fold (fun acc node linked ->
-        failwith ""
-    ) ColoredGraph.of *)
+    let link root ~along graph = 
+      let () = check_contains root.node graph in
+      let () = check_contains along.node graph in
+      let () = check_color root along in
+      let () = check_color along root in
+      let edge = create_edge root along in
+      add_edge edge graph
 end
